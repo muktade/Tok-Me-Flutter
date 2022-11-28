4a57e0cfc879666fa56248721f9d75151be804d8
muktadeer <muktadeer@gmail.com>
2022-11-29 03:20:16 +0600

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tokme/constant/colors.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TokMe',
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home:const Text('Hello World'),
+    );
+  }
 }
