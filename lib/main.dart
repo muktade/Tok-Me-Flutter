c2af4df0e0527f620c8e360a29f55b2d193ecd39
muktadeer <muktadeer@gmail.com>
2022-12-01 03:42:48 +0600

    registration ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:tokme/views/screens/auth/login_screen.dart';
 import 'package:tokme/views/screens/auth/registration_sereen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TokMe',
       theme: ThemeData(
